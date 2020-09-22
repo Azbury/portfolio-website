2307809696cc7e9c39ae2fbf0226dc56b210302b
azbury <acasbury25@aol.com>
2020-09-21 23:32:56 -0400

    changing header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
-  render () {
-    return (
-      <div>
-        <h1 className="title">Austin Asbury</h1>
-      </div>
-    )
-  }
+    render () {
+        return (
+          <div className="App">
+              <h1 className="title">Austin Asbury</h1>
+          </div>
+        )
+    }
 }
 
 export default App
