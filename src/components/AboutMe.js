1cd8271fd836fd6572135928ceadacd90941fa93
azbury <acasbury25@aol.com>
2020-09-27 17:32:06 -0400

    adding to about me paragraph

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,9 @@ class AboutMe extends Component {
         return (
             <div className="about-me">
                 <h2>About Me</h2>
-                <p>My name is Austin Asbury and I recently graduated from the Flatiron School for Software Engineering Bootcamp.</p>
+                <p>My name is Austin Asbury and I recently graduated from the Flatiron School for Software Engineering Bootcamp.
+                   I chose the self-paced program because I had prior experience coding from the University of Maryland located in College Park, MD where I majored in Computer Science.
+                </p>
             </div>
         )
     }
