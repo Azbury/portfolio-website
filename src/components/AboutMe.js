ea3e75a76e424b7f87ba1500dc676e4ed94562bf
azbury <acasbury25@aol.com>
2020-09-27 11:17:25 -0400

    add about me

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AboutMe extends Component {
+    render() {
+        return (
+            <div className="about-me">
+                <h2>About Me</h2>
+            </div>
+        )
+    }
+}
+
+export default AboutMe
