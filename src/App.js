572550cb700ffa7189b45db5224f89eb60dd7e5a
azbury <acasbury25@aol.com>
2020-09-27 09:35:26 -0400

    adding nav bar names

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 class App extends Component {
     render () {
         return (
           <div className="App">
-              <h1 className="title">Austin Asbury</h1>
+              <Header/>
           </div>
         )
     }
