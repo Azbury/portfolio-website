f680fd02db04b22958d0b3f65c6a5a8454bdfe3d
azbury <acasbury25@aol.com>
2020-09-29 20:58:32 -0400

    adding footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AboutMe from './components/AboutMe'
 import Header from './components/Header'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
+import Footer from './components/Footer'
 
 class App extends Component {
     render () {
@@ -13,6 +14,7 @@ class App extends Component {
               <AboutMe/>
               <Skills/>
               <Projects/>
+              <Footer/>
           </div>
         )
     }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <h1>Contact Info</h1>
+            </div>
+        )
+    }
+}
+
+export default Footer
