5dbc0c8882431bc73d7cfc28a27a915ac8e64081
azbury <acasbury25@aol.com>
2020-09-30 18:49:29 -0400

    changing position of picture and adding alt

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,8 +5,8 @@ class AboutMe extends Component {
     render() {
         return (
             <div className="about-me">
+                <img className="profile-pic" src={ProfilePic} alt={'profile pic'}/>
                 <h2>About Me</h2>
-                <img className="profile-pic" src={ProfilePic}/>
                 <p className="about-me-description">My name is Austin Asbury and I recently graduated from the Flatiron School for Software Engineering Bootcamp.
                    I chose the self-paced program because I had prior experience coding from the University of Maryland located in College Park, MD where I majored in Computer Science.
                    Completing the self-paced program meant I had no one to monitor my progress through the course work so I had to learn how to manage my time properly to complete the school
