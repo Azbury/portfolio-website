62cc2c8deabacb87f61ecc799684d312d419b166
azbury <acasbury25@aol.com>
2020-09-30 16:20:19 -0400

    centering about me paragraph

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ class AboutMe extends Component {
         return (
             <div className="about-me">
                 <h2>About Me</h2>
-                <p>My name is Austin Asbury and I recently graduated from the Flatiron School for Software Engineering Bootcamp.
+                <p className="about-me-description">My name is Austin Asbury and I recently graduated from the Flatiron School for Software Engineering Bootcamp.
                    I chose the self-paced program because I had prior experience coding from the University of Maryland located in College Park, MD where I majored in Computer Science.
                    Completing the self-paced program meant I had no one to monitor my progress through the course work so I had to learn how to manage my time properly to complete the school
                    work and meet deadlines that I set for myself while working full-time. This gave me a ton of practice for what it would be like to be working at a job with minimal supervision
