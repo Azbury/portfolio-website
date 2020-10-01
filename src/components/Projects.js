b36d708238b112d05ef0d72cbfe85c3bee228752
azbury <acasbury25@aol.com>
2020-09-30 20:45:54 -0400

    adding another video

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,7 @@ class Projects extends Component {
                 <h3>IQ Card Memory Game</h3>
                 <ReactPlayer className="video" url="https://www.youtube.com/watch?v=FtUPbcKe2Eg"/>
                 <h3>Creeper Aim Practice</h3>
+                <ReactPlayer className="video" url="https://www.youtube.com/watch?v=SiYIpxfAwmU"/>
                 <h3>Home Remedy Website</h3>
                 <h3>Online Marketplace Listings</h3>
                 <h3>Top 10 Hiking Spots in PA CLI</h3>
