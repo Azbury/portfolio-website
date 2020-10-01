f94968474fd0e3aa629a2a85916458065417bdda
azbury <acasbury25@aol.com>
2020-09-30 20:34:57 -0400

    adding youtube video for project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactPlayer from "react-player"
 
 class Projects extends Component {
     render() {
@@ -6,6 +7,7 @@ class Projects extends Component {
             <div className="projects">
                 <h2>Projects</h2>
                 <h3>IQ Card Memory Game</h3>
+                <ReactPlayer className="video" url="https://www.youtube.com/watch?v=FtUPbcKe2Eg"/>
                 <h3>Creeper Aim Practice</h3>
                 <h3>Home Remedy Website</h3>
                 <h3>Online Marketplace Listings</h3>
