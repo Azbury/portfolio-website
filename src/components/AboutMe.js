e81b2c0e50f92dccbe7e6caa2f51d292e53a8094
azbury <acasbury25@aol.com>
2020-10-04 14:41:28 -0400

    adjusting styling of about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,15 +5,18 @@ class AboutMe extends Component {
     render() {
         return (
             <div className="about-me">
-                <img className="profile-pic" src={ProfilePic} alt={'profile pic'}/>
-                <h2>About Me</h2>
-                <p className="about-me-description">My name is Austin Asbury and I recently graduated from the Flatiron School for Software Engineering Bootcamp.
-                   I chose the self-paced program because I had prior experience coding from the University of Maryland located in College Park, MD where I majored in Computer Science.
-                   Completing the self-paced program meant I had no one to monitor my progress through the course work so I had to learn how to manage my time properly to complete the school
-                   work and meet deadlines that I set for myself while working full-time. This gave me a ton of practice for what it would be like to be working at a job with minimal supervision
-                   and prepared me to become a developer that meets and exceeds deadlines given to them. I will do whatever it takes to make sure a project is completed on time, without
-                   compromising on quality.
-                </p>
+                <div className="pic">
+                    <img className="profile-pic" src={ProfilePic} alt={'profile pic'}/>
+                </div>
+                <div className="paragraph">
+                    <p className="about-me-description">My name is Austin Asbury and I recently graduated from the Flatiron School for Software Engineering Bootcamp.
+                    I chose the self-paced program because I had prior experience coding from the University of Maryland located in College Park, MD where I majored in Computer Science.
+                    Completing the self-paced program meant I had no one to monitor my progress through the course work so I had to learn how to manage my time properly to complete the school
+                    work and meet deadlines that I set for myself while working full-time. This gave me a ton of practice for what it would be like to be working at a job with minimal supervision
+                    and prepared me to become a developer that meets and exceeds deadlines given to them. I will do whatever it takes to make sure a project is completed on time, without
+                    compromising on quality.
+                    </p>
+                </div>
             </div>
         )
     }
