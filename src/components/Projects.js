025a6649bb0971bc085b9f119383498a16834174
azbury <acasbury25@aol.com>
2020-10-14 23:53:44 -0400

    dividing up projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,14 +6,26 @@ class Projects extends Component {
         return (
             <div className="projects">
                 <h2 className="projects-title">Projects</h2>
-                <h3>IQ Card Memory Game</h3>
-                <ReactPlayer className="video" url="https://www.youtube.com/watch?v=FtUPbcKe2Eg"/>
-                <h3>Creeper Aim Practice</h3>
-                <ReactPlayer className="video" url="https://www.youtube.com/watch?v=SiYIpxfAwmU"/>
-                <h3>Home Remedy Website</h3>
-                <ReactPlayer className="video" url="https://www.youtube.com/watch?v=x-FUyiKhJHE"/>
-                <h3>Online Marketplace Listings</h3>
-                <ReactPlayer className="video" url="https://www.youtube.com/watch?v=-tfIOoW3AjQ"/>
+                <div className="first-two-projects">
+                    <div className="iq-card-memory-game">
+                        <h3>IQ Card Memory Game</h3>
+                        <ReactPlayer className="video" url="https://www.youtube.com/watch?v=FtUPbcKe2Eg"/>
+                    </div>
+                    <div className="aim-game">
+                        <h3>Creeper Aim Practice</h3>
+                        <ReactPlayer className="video" url="https://www.youtube.com/watch?v=SiYIpxfAwmU"/>
+                    </div>
+                </div>
+                <div className="second-two-projects">
+                    <div className="home-remedy-website">
+                        <h3>Home Remedy Website</h3>
+                        <ReactPlayer className="video" url="https://www.youtube.com/watch?v=x-FUyiKhJHE"/>
+                    </div>
+                    <div className="online-marketplace-listings">
+                        <h3>Online Marketplace Listings</h3>
+                        <ReactPlayer className="video" url="https://www.youtube.com/watch?v=-tfIOoW3AjQ"/>
+                    </div>
+                </div>
                 <h3>Top 10 Hiking Spots in PA CLI</h3>
                 <ReactPlayer className="video" url="https://www.youtube.com/watch?v=WrHwM3LK9sE"/>
             </div>
