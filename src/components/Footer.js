ac8fe75e7b6bc268946068452cf144893ac164da
azbury <acasbury25@aol.com>
2020-10-21 17:15:56 -0400

    adding click for linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,14 @@ import MyComponent from "react-lite-button"
 
 class Footer extends Component {
 
-    handleClick(e) {
+    handleGithubClick(e) {
         e.preventDefault()
         window.open('https://github.com/Azbury', "_blank")
+    }
+
+    handleLinkedInClick(e) {
+        e.preventDefault()
+        window.open('https://www.linkedin.com/in/austin-asbury/', "_blank")
     }
 
     render() {
@@ -17,10 +22,10 @@ class Footer extends Component {
                     <li>acasbury25@aol.com</li>
                 </ul>
                 <div className="footer-github-btn">
-                    <MyComponent onClick={(event) => this.handleClick(event)}colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
+                    <MyComponent onClick={(event) => this.handleGithubClick(event)} colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
                 </div>
                 <div className="footer-linkedin-btn">
-                    <MyComponent colors={["#00008B" , "#87CEFA"]} width={200}>LinkedIn</MyComponent>
+                    <MyComponent onClick={(event) => this.handleLinkedInClick(event)} colors={["#00008B" , "#87CEFA"]} width={200}>LinkedIn</MyComponent>
                 </div>
             </div>
         )
