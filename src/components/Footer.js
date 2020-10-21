543c2d8195877564aab8872a658a20c5b62f46d4
azbury <acasbury25@aol.com>
2020-10-21 16:21:00 -0400

    adding github and linkedin button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import MyComponent from "react-lite-button"
 
 class Footer extends Component {
     render() {
@@ -9,6 +10,12 @@ class Footer extends Component {
                     <li>570-624-1138</li>
                     <li>acasbury25@aol.com</li>
                 </ul>
+                <div className="footer-github-btn">
+                    <MyComponent colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
+                </div>
+                <div className="footer-linkedin-btn">
+                    <MyComponent colors={["#00008B" , "#87CEFA"]} width={200}>LinkedIn</MyComponent>
+                </div>
             </div>
         )
     }
