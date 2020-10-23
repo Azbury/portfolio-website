294519369824137fbdad948a245dbd089a62fcd1
azbury <acasbury25@aol.com>
2020-10-23 15:45:41 -0400

    contact info will change color when moused over

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,11 @@ class Header extends Component {
         })
     }
 
+    mouse() {
+        var el = document.getElementById("contact-info")
+        el.style.color = "red"
+    }
+
     render() {
         return (
             <div className="header">
@@ -17,7 +22,7 @@ class Header extends Component {
                 <h2 className="navbar-link">About Myself</h2>
                 <h2 className="navbar-link">Skills</h2>
                 <h2 className="navbar-link">Projects</h2>
-                <h2 onClick={(event) => this.handleContactClick(event)}className="navbar-link">Contact Info</h2>
+                <h2 onMouseEnter={() => this.mouse()} onClick={(event) => this.handleContactClick(event)} className="navbar-link" id={"contact-info"}>Contact Info</h2>
             </div>
         )
     }
