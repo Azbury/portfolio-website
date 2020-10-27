0ed7397738133ee55f8950b3a908a52043f8934b
azbury <acasbury25@aol.com>
2020-10-27 11:10:45 -0400

    adding slideshow to display projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ReactPlayer from "react-player"
 import MyComponent from "react-lite-button"
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 class Projects extends Component {
 
@@ -34,6 +36,29 @@ class Projects extends Component {
             <div ref={this.props.scrollReference} className="projects">
                 <div className="divider"></div>
                 <h2 className="projects-title">Projects</h2>
+                <div className="slideshow-container">
+                    <Slide autoplay={false}>
+                        <div className="each-slide">
+                            <ReactPlayer className="video" url="https://www.youtube.com/watch?v=FtUPbcKe2Eg"/>
+                            <div className="project-description-section">  
+                                <p className="project-description">
+                                    This is a game built using React.js, HTML, CSS, with a Ruby on Rails API for the
+                                    backend. The game allows a user to enter a username and take them to their user page. 
+                                    They can they either start a new game or view the leaderboard page for the game. The 
+                                    game is an card memory matching game where they are given the time view the position 
+                                    of cards and then they have to flip them over and match them from their memory. A user 
+                                    gains 10 points for every correct answer, and loses 10 points for every wrong answer.
+                                </p>
+                                <div className="github-btn">
+                                    <MyComponent onClick={(event) => this.handleIQClick(event)} colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="each-slide">
+                            <ReactPlayer className="video" url="https://www.youtube.com/watch?v=SiYIpxfAwmU"/>
+                        </div>
+                    </Slide>
+                </div>
                 <div className="first-two-projects">
                     <div className="iq-card-memory-game">
                         <h3>IQ Card Memory Game</h3>
@@ -46,7 +71,7 @@ class Projects extends Component {
                             of cards and then they have to flip them over and match them from their memory. A user 
                             gains 10 points for every correct answer, and loses 10 points for every wrong answer.
                         </p>
-                        <div className="github-btn">
+                        <div>
                             <MyComponent onClick={(event) => this.handleIQClick(event)} colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
                         </div>
                     </div>
@@ -60,7 +85,7 @@ class Projects extends Component {
                             scores and will display their top 5 score to them in order. The user has one second click each creeper
                             that appears on the screen before it dissapears.
                         </p>
-                        <div className="github-btn">
+                        <div>
                             <MyComponent onClick={(event) => this.handleAimClick(event)} colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
                         </div>
                     </div>
@@ -75,7 +100,7 @@ class Projects extends Component {
                             of all remedies, post their own home remedies for everyone to see, and comment on others post
                             to let them know how they work.
                         </p>
-                        <div className="github-btn">
+                        <div>
                             <MyComponent onClick={(event) => this.handleRemedyClick(event)} colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
                         </div>
                     </div>
@@ -88,7 +113,7 @@ class Projects extends Component {
                             A user can list items with a title, description, and a price for the other users of the website to view.
                             A user passwords are encrypted using the has_secure_password method from ActiveRecord.
                         </p>
-                        <div className="github-btn">
+                        <div>
                             <MyComponent onClick={(event) => this.handleMarketClick(event)} colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
                         </div>
                     </div>
@@ -100,7 +125,7 @@ class Projects extends Component {
                     <p>
                         This Ruby Program provides a CLI to view the top 10 hiking spots in PA from the website onlyinyourstate based on article from 2015.
                     </p>
-                    <div className="github-btn">
+                    <div>
                             <MyComponent onClick={(event) => this.handleCLIClick(event)} colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
                     </div>
                 </div>
