40119dc151d02d337c310c5006b90c706c5f2576
azbury <acasbury25@aol.com>
2020-10-27 12:38:31 -0400

    adding second project to slideshow

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -39,23 +39,31 @@ class Projects extends Component {
                 <div className="slideshow-container">
                     <Slide autoplay={false}>
                         <div className="each-slide">
-                            <ReactPlayer className="video" url="https://www.youtube.com/watch?v=FtUPbcKe2Eg"/>
-                            <div className="project-description-section">  
-                                <p className="project-description">
-                                    This is a game built using React.js, HTML, CSS, with a Ruby on Rails API for the
-                                    backend. The game allows a user to enter a username and take them to their user page. 
-                                    They can they either start a new game or view the leaderboard page for the game. The 
-                                    game is an card memory matching game where they are given the time view the position 
-                                    of cards and then they have to flip them over and match them from their memory. A user 
-                                    gains 10 points for every correct answer, and loses 10 points for every wrong answer.
-                                </p>
-                                <div className="github-btn">
-                                    <MyComponent onClick={(event) => this.handleIQClick(event)} colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
-                                </div>
+                            <ReactPlayer className="video" url="https://www.youtube.com/watch?v=FtUPbcKe2Eg"/> 
+                            <p className="project-description">
+                                This is a game built using React.js, HTML, CSS, with a Ruby on Rails API for the
+                                backend. The game allows a user to enter a username and take them to their user page. 
+                                They can they either start a new game or view the leaderboard page for the game. The 
+                                game is an card memory matching game where they are given the time view the position 
+                                of cards and then they have to flip them over and match them from their memory. A user 
+                                gains 10 points for every correct answer, and loses 10 points for every wrong answer.
+                            </p>
+                            <div className="github-btn">
+                                <MyComponent onClick={(event) => this.handleIQClick(event)} colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
                             </div>
                         </div>
                         <div className="each-slide">
                             <ReactPlayer className="video" url="https://www.youtube.com/watch?v=SiYIpxfAwmU"/>
+                            <p className="project-description">
+                                This is a game built using JavaScript, HTML, CSS, with a Ruby on Rails API for the backend. 
+                                The game allows a user to enter a username and then start a new game where they click on creepers 
+                                to gain points and practice their aim for shooting video games. The program will keep track of their 
+                                scores and will display their top 5 score to them in order. The user has one second click each creeper
+                                that appears on the screen before it dissapears.
+                            </p>
+                            <div className="github-btn">
+                                <MyComponent onClick={(event) => this.handleAimClick(event)} colors={["#000000" , "#808080"]} width={200}>Github</MyComponent>
+                            </div>
                         </div>
                     </Slide>
                 </div>
