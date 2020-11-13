91025fc981c20a9b8cbd38c97a56b974f5c0d934
azbury <acasbury25@aol.com>
2020-11-13 14:18:34 -0500

    adding in job responsiblities

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,6 +6,15 @@ class Experience extends Component {
             <div className="experience">
                 <div className="divider"/>
                 <h2 id="experience-title">Experience</h2>
+                <ul>
+                    <li>Designing, testing, and modifying websites and digital media within a team environment.</li>
+                    <li>Assumes responsibility for all website testing and design modification.</li>
+                    <li>Develop all applicable unit testing and documentation to ensure expected functionality.</li>
+                    <li>Ensure compatibility across all supported browser versions.</li>
+                    <li>Participate in code reviews.</li>
+                    <li>Recommend and implement improvements to existing web applications and components.</li>
+                    <li>Assumes responsibility for understanding current and new web technologies.</li>
+                </ul>
                 <div className="divider"/>
             </div>
         )
