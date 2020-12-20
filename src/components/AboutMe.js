dd4eff5fd8e52146f476866bbd11e367fbfc9696
azbury <acasbury25@aol.com>
2020-12-19 21:37:21 -0500

    comptetive person

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -17,7 +17,7 @@ class AboutMe extends Component {
                         <p className="about-me-description">My name is Austin Asbury and I am a Jr. UX/ UI Developer at Ritter Insurance Marketing.
                         I studied computer science at the University of Maryland as well as completed the Flatiron School for Software Engineering bootcamp.
                         Skiing and video games are by far my two favorites hobbies and if I am not coding you will either see me on the slopes, or in a
-                        competitive video game.
+                        competitive video game. I am a competitive person, so I will always be striving the be the best at anything I do in life.
                         Working from home with my two cats Momo and Yuumi has been an experience that I have been looking forward to since I first started studying
                         computer science back in college. I am an extremely motivated and hard working individual who requires minimal supervision to complete work
                         at a high level. It feels amazing to be able to wake up everyday and get to do something I love and enjoy.
